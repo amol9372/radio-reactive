3b3e610b7c3821ebdd9e842f370bd0154f84564f
amol9372 <amolsingh9372@gmail.com>
2021-03-22 10:25:57 +0530

    sample commit

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -5,8 +5,9 @@ import Person from "./components/person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Amol", age: 34 },
-      { name: "Robin", age: 45 },
+      { id: "1", name: "Amol", age: 34 },
+      { id: "2", name: "Robin", age: 45 },
+      { id: "3", name: "vikas", age: 53 },
     ],
   };
 
@@ -34,9 +35,9 @@ class App extends Component {
         <h2>This is test app</h2>
 
         <div className="App">
-          {this.state.persons.map((person) => (
+          {this.state.persons.map((person, index) => (
             <Person
-              key={person.name}
+              key={person.id}
               name={person.name}
               age={person.age}
               click={this.personSwapHandler}
